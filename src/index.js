a687e8003315e97f281c5ea3e50b2909a65bce08
Dumik121 <nazar121duma@gmail.com>
2023-02-15 12:53:13 +0200

    feat: brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(string, brackets) {
+  const pairs = new Map(brackets);
+  const stack = [];
+  for (const char of string) {
+    if (pairs.get(stack.at(-1)) == char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length == 0;
+};
